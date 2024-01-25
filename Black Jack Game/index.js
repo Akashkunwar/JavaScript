a38d7057a2f9b3132bb467b8e9d0b27a9b6ec09c
Akash Kumar <gnspda@gmail.com>
2024-01-25 22:56:54 +0530

    Complete Blackjack

--- Black Jack Game/index.js
+++ Black Jack Game/index.js
@@ -1,5 +1,3 @@
-// let firstCard = getRandomCard()
-// let secondCard = getRandomCard()
 let allCards = []
 let sum = 0
 let hasBlackJack = false
@@ -7,7 +5,6 @@ let isAlive = false
 let message = ""
 let messageEl = document.getElementById('message-el')
 let cardEl = document.getElementById('card-el')
-// let sumEl = document.getElementById("sum-el")
 let sumEl = document.querySelector("#sum-el")
 let userEl =document.querySelector('#user-el')
 
@@ -19,12 +16,10 @@ function startGame() {
     let firstCard = getRandomCard()
     let secondCard = getRandomCard()
     sum = firstCard + secondCard
-    allCards.push(firstCard)
-    allCards.push(secondCard)
+    allCards = [firstCard, secondCard]
     isAlive = true
     renderGame()
 }
-
 
 function getRandomCard() {
     let randonNumber = Math.floor(Math.random()*13)+1
@@ -38,18 +33,11 @@ function getRandomCard() {
 }
 
 function renderGame() {
-    // cardEl.textContent = `Cards: ${firstCard} ${secondCard}`;
-    // let cardInString = ''
-    // for (let i=0; i<allCards.length; i++) {
-    //     cardInString+=allCards[i]+" "
-    // }
-    // cardEl.textContent = `Cards: ${allCards[0]} ${allCards[1]}`;
     userEl.textContent = `${user.name} : $${user.chips}`
     cardEl.textContent = "Cards: "
     for (let i=0; i<allCards.length; i++) {
         cardEl.textContent+=allCards[i]+" "
     }
-    // cardEl.textContent = `Cards: ${cardInString}`;
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
@@ -64,7 +52,6 @@ function renderGame() {
 }
 
 function newCard() {
-    // console.log("Drawing new card.")
     if (isAlive===true && sum!==21) {
     let card = getRandomCard()
     allCards.push(card)
@@ -72,5 +59,3 @@ function newCard() {
     renderGame()}
 
 }
-// 3. Log it out to check that you're doing it right
-
