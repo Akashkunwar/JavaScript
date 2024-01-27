ff6f7cdd79d4c23736494822bb6f516218cef331
Akash <gnspda@gmail.com>
2024-01-27 15:22:28 +0530

    Added extension

--- URL Saver Extension/index.js
+++ URL Saver Extension/index.js
@@ -2,19 +2,26 @@
 //     console.log("Button Clicked and onclick responsing")
 // }
 let tempLead = 'www.demo.com'
-let myLeads = []
+let myLinks = []
 let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById('input-btn')
 let ulEl = document.getElementById('ul-el')
 
-function renderAll () {
-    ulEl.innerHTML = '';
-    for (let i = 0; i<myLeads.length; i++) {
-        ulEl.innerHTML += '<li>'+myLeads[i] +'</li>'
+function renderLeads () {
+    let listItem = ""
+    for (let i = 0; i<myLinks.length; i++) {
+        listItem += `
+        <li>
+            <a target="_blank" href="${myLinks[i]}">
+                ${myLinks[i]}
+            </a>
+        </li>` 
     }
+    ulEl.innerHTML=listItem
 }
 
 inputBtn.addEventListener('click', function () {
-    myLeads.push(inputEl.value)
-    renderAll()
+    myLinks.push(inputEl.value)
+    renderLeads()
+    inputEl.value = ''
 })
